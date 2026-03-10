c510a1eb9869197699e4438b3d49c02399d44980
Tor Arne Håve <torarnehave@gmail.com>
2026-03-10 18:11:35 +0100

    feat: refactor AgentBuilder component to improve chat view rendering logic and conditional display of HtmlPreview

--- src/components/AgentBuilder.tsx
+++ src/components/AgentBuilder.tsx
@@ -59,22 +59,9 @@ export default function AgentBuilder({ userId, userEmail, language, onLanguageCh
         </div>
       </header>
 
-      {view === 'chat' && !previewHtml && (
-        <AgentChat
-          userId={userId}
-          graphId={graphId}
-          onGraphChange={setGraphId}
-          agentId={selectedAgentId}
-          agentAvatarUrl={null}
-          onPreview={setPreviewHtml}
-          consoleErrors={consoleErrors}
-          onConsoleErrorsHandled={() => setConsoleErrors(null)}
-          onActiveHtmlNode={setActiveHtmlNodeId}
-        />
-      )}
-      {view === 'chat' && previewHtml && (
-        <div className="flex flex-1 min-h-0">
-          <div className="w-[40%] min-w-[320px] flex flex-col min-h-0 border-r border-white/10">
+      {view === 'chat' && (
+        <div className={`flex flex-1 min-h-0 ${previewHtml ? '' : ''}`}>
+          <div className={previewHtml ? 'w-[40%] min-w-[320px] flex flex-col min-h-0 border-r border-white/10' : 'flex-1 flex flex-col min-h-0'}>
             <AgentChat
               userId={userId}
               graphId={graphId}
@@ -87,15 +74,17 @@ export default function AgentBuilder({ userId, userEmail, language, onLanguageCh
               onActiveHtmlNode={setActiveHtmlNodeId}
             />
           </div>
-          <div className="flex-1 flex min-w-0">
-            <HtmlPreview
-              html={previewHtml}
-              onClose={() => { setPreviewHtml(null); setActiveHtmlNodeId(null); }}
-              onConsoleErrors={setConsoleErrors}
-              graphId={graphId}
-              nodeId={activeHtmlNodeId}
-            />
-          </div>
+          {previewHtml && (
+            <div className="flex-1 flex min-w-0">
+              <HtmlPreview
+                html={previewHtml}
+                onClose={() => { setPreviewHtml(null); setActiveHtmlNodeId(null); }}
+                onConsoleErrors={setConsoleErrors}
+                graphId={graphId}
+                nodeId={activeHtmlNodeId}
+              />
+            </div>
+          )}
         </div>
       )}
       {view === 'data' && <DataExplorer />}
